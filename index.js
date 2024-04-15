d51703ad098a83cdd80770ce5891888499cc3339
= <jan.sernec@gmail.com>
2024-04-16 01:11:18 +0200

    added custom api endpoint for posting tickets_provider_service

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const express = require('express');
 const grpcRouter = require('./grpcRouter');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-
 
 // Define proxy middleware for each microservice
 const usersProxy = createProxyMiddleware({
@@ -22,6 +22,22 @@ const purchasesProxy = createProxyMiddleware({
     changeOrigin: true,
     pathRewrite: {
         '^/api/purchases': '/api/purchases'
+    }
+});
+
+app.use('/api/purchases', async (req, res, next) => {
+    if (req.method === 'POST') {
+        try {
+            console.log('Request received:', req.method, req.url);
+            const response = await axios.post('http://tickets_provider_service:8084/api/purchases', req.body);
+            console.log("Response from fetch", response.data);
+            res.json(response.data);
+        } catch (error) {
+            console.error('Error:', error);
+            res.status(500).json({ error: 'Internal Server Error' });
+        }
+    } else {
+        next();
     }
 });
 
